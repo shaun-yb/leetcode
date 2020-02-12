a05b64bd230b9c6c73f49fb5c5d09bebc09e32b9
shaunlawrencecarland <58670562+shaunlawrencecarland@users.noreply.github.com>
2020-02-12 10:54:27 -0500

    Update 79_word_search.rb

--- 79_word_search.rb
+++ 79_word_search.rb
@@ -1,4 +1,3 @@
-# TODO: make this passing for all test cases
 # https://leetcode.com/problems/word-search/
 
 # @param {Character[][]} board
@@ -7,11 +6,11 @@
 def exist(board, word)
     rows = board.length - 1
     cols = board[0].length - 1
-    used = Array.new(board.length) { Array.new(board[0].length) }
+    used = Array.new(board.length) { Array.new(board[0].length) { false } }
     
     (0..board.length-1).each do |i|
         (0..board[0].length-1).each do |j|
-            return true if search(board,word,used,i,j)
+            return true if board[i][j] == word[0] && search(board,word,used,i,j)
         end
     end
     
@@ -19,21 +18,26 @@ def exist(board, word)
 end
 
 def search(board, word, used, i, j)
-    puts "word: #{word} i: #{i} j: #{j} word is empty? "
     return true if word.empty?
 
     return false if i > board.length - 1
     return false if i < 0
     return false if j > board[0].length - 1
     return false if j < 0
+    
     return false if used[i][j]
     return false if board[i][j] != word[0]
     
     used[i][j] = true
     word = word[1..-1]
     
-    search(board, word, used, i+1, j) ||
+    if search(board, word, used, i+1, j) ||
     search(board, word, used, i, j+1) ||
     search(board, word, used, i-1, j) ||
     search(board, word, used, i, j-1)
+        return true
+    end
+    
+    used[i][j] = false
+    return false
 end

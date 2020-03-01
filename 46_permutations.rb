7024ff2a2a3075b2bd1b4449855586800e47b64f
shaunlawrencecarland <58670562+shaunlawrencecarland@users.noreply.github.com>
2020-03-01 16:41:52 -0500

    Create 46_permutations.rb

---
+++ 46_permutations.rb
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/permutations/
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+    p(nums)
+end
+
+def p(nums)
+    if nums.length == 1
+        return [nums]
+    else
+        return merge(nums[0], p(nums[1..nums.length-1]))
+    end
+end
+
+def merge(num, num_sets)
+    new_num_sets = []
+    num_sets.each do |num_set|
+        (0..num_set.length).each do |i|
+            new_set = num_set.dup
+            new_set.insert(i, num)
+            new_num_sets << new_set
+        end
+    end
+    
+    new_num_sets
+end
